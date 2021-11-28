286b6cca109a415cc2f53e43739365bfa620a0a7
Mamoon Abdul Qayyum <mamoonabdulqayyum@gmail.com>
2021-11-28 12:08:40 +0500

    Add files via upload

---
+++ documentWrite.js
@@ -0,0 +1,5 @@
+document.write("Hello World.");
+document.write("This text is displayed using Document.write().");
+document.write(789096);
+document.write(".Error 404.");
+document.write(" Your order No. " + 679 +" will be served at "+ 9 + "pm.");
