beae00936688e4a16057d328a9a93154d4acdf43
Mamoon Abdul Qayyum <mamoonabdulqayyum@gmail.com>
2021-11-28 12:55:18 +0500

    Add files via upload

---
+++ Variables(var).js
@@ -0,0 +1,20 @@
+var nationality = "Pakistani";
+var age = 24;
+var temperature = 22.8;
+var isFeePaid = true;   
+//isFeePaid is a camel case name convention for variables in JS.
+//Rules for JS variable name: 1.No number in the start of variable name except underscore "_", dollar sign "$" and alphabets
+//JS is case sensitive so the variable are also.
+
+alert("Your nationality is "+ nationality);
+nationality = "Turkish";
+alert("Your nationality is "+ nationality);
+alert("Your age is "+ age);
+age = 65;
+alert("Your age is "+ age);
+alert("Your room temperature is " + temperature);
+temperature = 30;
+alert("Your room temperature is " + temperature);
+alert("Is your fee paid? " + "\n" + isFeePaid);
+isFeePaid = false;
+alert("Is your fee paid? " + "\n" + isFeePaid);
