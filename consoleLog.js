4678826cd178e989120468465c08901f5cddb767
Mamoon Abdul Qayyum <mamoonabdulqayyum@gmail.com>
2021-11-28 10:06:12 +0500

    Add files via upload

---
+++ consoleLog.js
@@ -0,0 +1,6 @@
+console.log("Hello World!");
+console.log("This is console.log() testing code");
+console.log("You've got " + 2 + " chicken burger and " + 1 + " regular coke.");
+console.log("1234567890");
+console.log(78654223);
+console.log("Error " + 404);
