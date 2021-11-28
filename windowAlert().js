ddf35150e5a4ca050d3492885c55f197c4a27f89
Mamoon Abdul Qayyum <mamoonabdulqayyum@gmail.com>
2021-11-28 09:54:01 +0500

    Add files via upload

---
+++ windowAlert().js
@@ -0,0 +1,10 @@
+window.alert("Hello World!");
+window.alert(123456789);
+window.alert("lorem ispum dummy text");
+window.alert("This is windows.alert() practice in JS");
+window.alert("The quick brown fox jumps over the lazy dog");
+window.alert("Your flight number is " + 9000765);
+window.alert("Your " + 2 + " pizzas will be served at " + 8 + " pm");
+window.alert("Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.");
+window.alert("Great people to fly with");
+window.alert("Error " + 404);
